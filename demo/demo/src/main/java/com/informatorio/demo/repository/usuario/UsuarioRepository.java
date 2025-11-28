57ad13ef9e1b09d016b0b3dd48d3c41af76a215a
Gerardo-Rioss <gerarios26@gmail.com>
2025-11-28 12:20:41 -0300

    update query method

--- demo/demo/src/main/java/com/informatorio/demo/repository/usuario/UsuarioRepository.java
+++ demo/demo/src/main/java/com/informatorio/demo/repository/usuario/UsuarioRepository.java
@@ -1,11 +1,14 @@
 package com.informatorio.demo.repository.usuario;
 
 import com.informatorio.demo.model.Usuario;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
     Optional<Usuario> findByEmail(String email);
-}
+    List<Usuario> findAllByNombreStartingWith(String nombre);
+    }
